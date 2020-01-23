72e4b98337012d6173d3a96ae59357bedb385b0f
JKRyan <jed@jedkendall.com>
2020-01-23 14:56:35 -0600

    More file structure and practice

--- engine.py
+++ engine.py
@@ -2,25 +2,35 @@
 import tcod as libtcod
 from modules.input_handlers import handle_keys
 from modules.entity import Entity
-from modules.render_functions import *
+from modules.render_functions import render_all, clear_all, draw_entity, clear_entity
+from modules.map_objects.game_map import GameMap
 
 
 def main():
     '''The loop that keeps the game running'''
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
+
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
     entities = [npc, player]
 
     libtcod.console_set_custom_font(
-        './data/fonts/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+        './dungeoncrash/data/fonts/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
     libtcod.console_init_root(
         screen_width, screen_height, 'Dungeoncrash', False)
 
     con = libtcod.console_new(screen_width, screen_height)
+
+    game_map = GameMap(map_width, map_height)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -28,7 +38,7 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
         
-        render_all(con, entities, screen_width, screen_height)
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
 
         libtcod.console_flush()
 
@@ -42,7 +52,8 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx, dy)
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
 
         if exit:
             return True
---
+++ modules/map_objects/tile.py
@@ -0,0 +1,11 @@
+'''Code for tiles'''
+class Tile:
+    '''A tile on a map.'''
+    def __init__(self, blocked, block_sight=None):
+        self.blocked = blocked
+
+        #By default. If a tile is blocked, it also blocks sight
+        if block_sight is None:
+            block_sight = blocked
+
+        self.block_sight = block_sight

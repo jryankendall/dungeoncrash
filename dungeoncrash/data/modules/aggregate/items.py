c017d1ecdc6fba462bb7e2799a5444d581b1cd41
JKRyan <jed@jedkendall.com>
2020-01-27 14:12:07 -0600

    More item aggregator work, now it uses folder path to determine category. Some more map practice, practice and testing on map generation and LOS

--- dungeoncrash/data/modules/aggregate/items.py
+++ dungeoncrash/data/modules/aggregate/items.py
@@ -1,20 +1,63 @@
 '''Aggregates item files in data/libs/items'''
 import json
 import os
+import re
 
-def pack_folder(folder):
+def pack_folder(folder="./dat"):
+    '''
+    Takes a string containing a relative path to a folder containing multiple .json files,
+    combines them into one array, returns that array. Uses the containing folder to apply
+    a category to the item as well.
+    '''
     full_list = []
     contents = os.listdir(folder)
+
+    item_errors = 0
+    err_files = []
     for file in contents:
         this_file = folder + file
-        items = json.load(open(this_file))
-        full_list.append(items)
-    write_list(full_list, "items.json")
+        with open(this_file) as current_file:
+            try:
+                items = json.load(current_file)
+            except ValueError:
+                items = {"flags": ["null"]}
+                item_errors = item_errors + 1
+                err_files.append(file)
+            category = strip_path(["libs/", "dat/"], folder, 1).lstrip("./")
+            items['category'] = category
+            full_list.append(items)
+    print("File read errors: " + str(item_errors))
+
+    if item_errors > 0:
+        print("Files with errors:")
+        print(err_files)
+
+    return full_list
 
 
-def write_list(items, name):
-    with open(name, mode="w") as f:
-        json.dump(items, f, indent=4)
+def write_list(items, name='items.json'):
+    '''
+    Takes an array of JSON objects, writes them to a new .json file.
+    '''
+    with open(name, mode="w") as f_pointer:
+        json.dump(items, f_pointer, indent=4)
         print("Successfully wrote to file: " + name)
 
-pack_folder('../../libs/items/gear/worn/armor/heavy/dat/')
+def strip_path(regex, path, times):
+    '''
+    Takes a string, makes a regex from it, strips path of it.
+    If regex is a list of strings, strips path of each one once, in order
+    '''
+    if isinstance(regex, list):
+        for r in regex:
+            pattern = re.compile(r)
+            path = pattern.sub("", path, count=1)
+        return path
+
+    else:
+        pattern = re.compile(regex)
+        return pattern.sub("", path, count=times)
+
+# Below is mostly for testing purposes, remove later.
+write_list(items=pack_folder(folder='../../libs/items/gear/worn/armor/light/dat/'),
+           name="items_test.json")
--- modules/map_objects/game_map.py
+++ modules/map_objects/game_map.py
@@ -1,6 +1,7 @@
 '''Map example'''
 from modules.map_objects.tile import Tile
 from modules.map_objects.rectangle import Rect
+from random import randint
 
 
 class GameMap:
@@ -20,16 +21,56 @@ class GameMap:
                 self.tiles[x][y].blocked = False
                 self.tiles[x][y].block_sight = False
 
+    def create_h_tunnel(self, x1, x2, y):
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1, y2, x):
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
             return True
 
         return False
         
-    def make_map(self):
-            #Create two rooms
-            room1 = Rect(20, 15, 10, 15)
-            room2 = Rect(35, 15, 10, 15)
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+        rooms = []
+        num_rooms = 0
 
-            self.create_room(room1)
-            self.create_room(room2)
+        for r in range(max_rooms):
+            # random width and height
+            w = randint(room_min_size, room_max_size)
+            h = randint(room_min_size, room_max_size)
+            #random pos without going out of the map
+            x = randint(0, map_width - w - 1)
+            y = randint(0, map_height - h - 1)
+
+            new_room = Rect(x, y, w, h)
+
+            for other_room in rooms:
+                if new_room.intersect(other_room):
+                    break
+            else:
+                self.create_room(new_room)
+
+                (new_x, new_y) = new_room.center()
+
+                if num_rooms == 0:
+                    player.x = new_x
+                    player.y = new_y
+                else:
+                    (prev_x, prev_y) = rooms[num_rooms -1].center()
+
+                    if randint(0, 1) == 1:
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+                    else:
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+
+                rooms.append(new_room)
+                num_rooms += 1
